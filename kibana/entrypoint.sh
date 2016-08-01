3ebd3731b744ff38611bcb15cb178d78ff69040a
Wilhelm Uschtrin <uschtrinw@gmail.com>
2016-08-01 10:41:44 +0200

    fixes Kibana memory leak

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -1,10 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
 
-# Wait for the Elasticsearch container to be ready before starting Kibana.
-echo "Stalling for Elasticsearch"
-while true; do
-    nc -q 1 elasticsearch 9200 2>/dev/null && break
-done
+# Add kibana as command if needed
+if [[ "$1" == -* ]]; then
+  set -- kibana "$@"
+fi
 
-echo "Starting Kibana"
-exec kibana
+# Run as user "kibana" if the command is "kibana"
+if [ "$1" = 'kibana' ]; then
+  if [ "$ELASTICSEARCH_URL" ]; then
+    sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
+  fi
+
+  set -- gosu kibana tini -- "$@"
+fi
+
+exec "$@"
