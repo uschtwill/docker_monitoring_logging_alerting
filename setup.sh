07fbd526b1d166a7ba4bea26fa6c505165684132
Wilhelm Uschtrin <uschtrinw@gmail.com>
2016-09-05 16:30:04 +0200

    pre-pulls images and adds --force-recreaete and --build tags for docker-compose up command

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 echo --------------------------- Creating separate docker network...
 docker network create --subnet=192.16.0.0/24 monitoring_logging
 echo --------------------------- Starting \(incl. pulling/building images\) ACTUAL containers...
-docker-compose up -d
+docker-compose pull
+docker-compose up -d --force-recreate --build
 echo --------------------------- Output from 'docker ps'...
 docker ps
 echo ---------------------------
