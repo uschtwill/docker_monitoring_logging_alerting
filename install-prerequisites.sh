ece5bacc8a297f7ba0eea0701e4fdcd300259241
Wilhelm Uschtrin <uschtrinw@gmail.com>
2016-12-27 11:30:50 +0100

    adds a script to install prerequisites on tabula rasa boxes

---
+++ install-prerequisites.sh
@@ -0,0 +1,3 @@
+curl -sSL https://get.docker.com/ | sh
+curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
