acb24a66ae29057bdd5c48f28e2acc702bf838b0
Wilhelm Uschtrin <uschtrinw@gmail.com>
2016-12-27 14:34:14 +0100

    vm.max_map_count=262144 on host for Elasticsearch 5.0

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,3 +1,4 @@
 curl -sSL https://get.docker.com/ | sh
 curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+sudo sysctl -w vm.max_map_count=262144
