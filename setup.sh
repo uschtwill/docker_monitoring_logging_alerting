5ec132c2e1b848719edcd2ef131cb90ec774d4ae
Wilhelm Uschtrin <uschtrinw@gmail.com>
2016-12-28 15:06:19 +0100

    first push of nginx ssl/basic-auth reverse proxy implementation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,30 @@
+echo ---------------------------
+
+echo --------------------------- Setting domain and password...
+export DOMAIN=$1
+export PASSWORD=$2
+
+echo --------------------------- Setting passwords for basic auth...
+htpasswd -bc storage/nginx-proxy/htpasswd/kibana.$1 admin $2
+# htpasswd -bc storage/nginx-proxy/htpasswd/grafana.$1 admin $2 # See: https://github.com/grafana/grafana/issues/7090
+htpasswd -bc storage/nginx-proxy/htpasswd/prometheus.$1 admin $2
+htpasswd -bc storage/nginx-proxy/htpasswd/alertmanager.$1 admin $2
+
 echo --------------------------- Creating separate docker network...
 docker network create --subnet=192.16.0.0/24 monitoring_logging
+
 echo --------------------------- Pulling images...
 docker-compose pull
+
 echo --------------------------- Building images...
 docker-compose build
+
 echo --------------------------- Starting containers...
-docker-compose up -d --force-recreate --build
+docker-compose up --force-recreate
+
+echo --------------------------- Unsetting exported environment variables...
+unset DOMAIN
+unset PASSWORD
+
 echo --------------------------- Output from 'docker ps'...
 docker ps
-echo ---------------------------
