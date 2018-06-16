58bfb0c63d72ee943000b98e43aa3515bfd6f61b
Wilhelm Uschtrin <uschtrinw@gmail.com>
2018-06-16 22:52:07 +0200

    using $(...) instead of legacy `...`. shoutout to https://github.com/koalaman/shellcheck

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -16,7 +16,7 @@ curl -sSL https://get.docker.com/ | sh
 echo "------------------------------------------------------------"
 echo "############################### Downloading docker-compose..."
 echo "------------------------------------------------------------"
-curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-"$(uname -s)"-"$(uname -m)" > /usr/local/bin/docker-compose
 
 echo "------------------------------------------------------------"
 echo "############################### Making docker-compose executable..."
